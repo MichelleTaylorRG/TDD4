4de557e04ec886855f42f78edb7aca067400452d
MichelleTaylorRG <michelle.taylor@red-gate.com>
2017-05-26 14:41:11 +0100

    simplify structure and add draws

--- TDD4/Game.cs
+++ TDD4/Game.cs
@@ -5,24 +5,25 @@ namespace TDD4
 {
     public class Game
     {
-        public List<RoundResult> roundResults = new List<RoundResult>();
+        public int Player1Wins { get; set; }
+
+        public int Player2Wins { get; set; }
+
+        public int Draws { get; set; }
 
         public GameWinner GetWinner()
         {
-            if (roundResults.Count < 3)
+            if (Player1Wins + Player2Wins + Draws  < 3)
             {
                 return GameWinner.Undecided;
             }
 
-            var player1Wins = roundResults.Count(x => x == RoundResult.Player1Win);
-            var player2Wins = roundResults.Count(x => x == RoundResult.Player2Win);
-
-            if (player1Wins > player2Wins)
+            if (Player1Wins == Player2Wins)
             {
-                return GameWinner.Player1;
+                return GameWinner.Undecided;
             }
 
-            return GameWinner.Player2;
+            return Player1Wins > Player2Wins ? GameWinner.Player1 : GameWinner.Player2;
         }
     }
 }
--- TDD4/GameWinnerTests.cs
+++ TDD4/GameWinnerTests.cs
@@ -12,98 +12,15 @@ namespace TDD4
     [TestFixture]
     public class GameWinnerTests
     {
-        private IList<RoundResult> generateRounds(int player1Score, int player2Score, int draws)
-        {
-            var roundResults = new List<RoundResult>();
-
-            var player1WinsGenerated = 0;
-            var player2WinsGenerated = 0;
-            var drawsGenerated = 0;
-            var randomGenerator = new Random();
-            for (var i = 0; i < player1Score + player2Score + draws; i++)
-            {
-                if (player1Score > player1WinsGenerated)
-                {
-                    if (player2Score > player2WinsGenerated)
-                    {
-                        if (draws > drawsGenerated)
-                        {
-                            var randomSelection = randomGenerator.Next(2);
-                            switch (randomSelection)
-                            {
-                                case 0:
-                                    roundResults.Add(RoundResult.Draw);
-                                    drawsGenerated++;
-                                    break;
-                                case 1:
-                                    roundResults.Add(RoundResult.Player1Win);
-                                    player1WinsGenerated++;
-                                    break;
-                                case 2:
-                                    roundResults.Add(RoundResult.Player2Win);
-                                    player2WinsGenerated++;
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            var randomSelection = randomGenerator.Next(1);
-                            switch (randomSelection)
-                            {
-                                case 1:
-                                    roundResults.Add(RoundResult.Player1Win);
-                                    player1WinsGenerated++;
-                                    break;
-                                case 2:
-                                    roundResults.Add(RoundResult.Player2Win);
-                                    player2WinsGenerated++;
-                                    break;
-                            }
-                        }
-                    }
-                    roundResults.Add(RoundResult.Player1Win);
-                    player1WinsGenerated++;
-                }
-                else if (player2Score > player2WinsGenerated)
-                {
-                    if (draws < drawsGenerated)
-                    {
-                        var randomSelection = randomGenerator.Next(1);
-                        switch (randomSelection)
-                        {
-                            case 0:
-                                roundResults.Add(RoundResult.Draw);
-                                drawsGenerated++;
-                                break;
-                            case 1:
-                                roundResults.Add(RoundResult.Player2Win);
-                                player2WinsGenerated++;
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        roundResults.Add(RoundResult.Player2Win);
-                        player2WinsGenerated++;
-                    }
-                    throw new Exception("I ran out of things to generate before I ran out of rounds!");
-                }
-                else if (draws > drawsGenerated)
-                {
-                    roundResults.Add(RoundResult.Draw);
-                    drawsGenerated++;
-                }
-            }
-            return roundResults;            
-        }
-
-
         [TestCase(0, 3, GameWinner.Player2)]
         [TestCase(3, 0, GameWinner.Player1)]
         public void ThreeWonRoundsWinGame(int player1Score, int player2Score, GameWinner expectedWinner)
         {
-            var game = new Game();
-            game.roundResults.AddRange(generateRounds(player1Score,player2Score,0));
+            var game = new Game
+            {
+                Player1Wins = player1Score,
+                Player2Wins = player2Score
+            };
             game.GetWinner().Should().Be(expectedWinner);
         }
         
@@ -114,20 +31,49 @@ namespace TDD4
         [TestCase(0, 1)]
         [TestCase(2, 2)]
         [TestCase(1, 1)]
-        public void LessThanThreeWonRoundsShouldBeUndecided(int player1Score, int player2Score)
+        public void LessThanThreeRoundsWithNoDrawsShouldBeUndecided(int player1Score, int player2Score)
         {
-            var game = new Game();
-            game.roundResults.AddRange(generateRounds(player1Score, player2Score, 0));
+            var game = new Game
+            {
+                Player1Wins = player1Score,
+                Player2Wins = player2Score
+            };
             game.GetWinner().Should().Be(GameWinner.Undecided);
         }
 
         [TestCase(1, 2, GameWinner.Player2)]
         [TestCase(2, 1, GameWinner.Player1)]
-        public void ClearWinnerAfterThreeRoundsShouldWin(int player1Score, int player2Score, GameWinner expectedWinner)
+        public void ClearWinnerAfterThreeRoundsWithNoDrawsShouldWin(int player1Score, int player2Score, GameWinner expectedWinner)
+        {
+            var game = new Game
+            {
+                Player1Wins = player1Score,
+                Player2Wins = player2Score
+            };
+            game.GetWinner().Should().Be(expectedWinner);
+        }
+
+        [TestCase(1, 2, 1, GameWinner.Player2)]
+        [TestCase(1, 0, 1, GameWinner.Undecided)]
+        [TestCase(1, 1, 1, GameWinner.Undecided)]
+        [TestCase(2, 0, 1, GameWinner.Player1)]
+        [TestCase(2, 1, 1, GameWinner.Player1)]
+        public void FindCorrectWinnerAfterThreeRoundsWithDraws(int player1Score, int player2Score, int draws, GameWinner expectedWinner)
+        {
+            var game = new Game
+            {
+                Player1Wins = player1Score,
+                Player2Wins = player2Score,
+                Draws = draws
+            };
+            game.GetWinner().Should().Be(expectedWinner);
+        }
+
+        [Test]
+        public void GameWithoutRoundsShouldBeUndecided()
         {
             var game = new Game();
-            game.roundResults.AddRange(generateRounds(player1Score, player2Score, 0));
-            game.GetWinner().Should().Be(expectedWinner);
+            game.GetWinner().Should().Be(GameWinner.Undecided);
         }
     }
 }
