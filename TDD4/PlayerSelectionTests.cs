6c6d20ce88434ab33842cfb62a69b7d45fa4fc0d
MichelleTaylorRG <michelle.taylor@red-gate.com>
2017-05-26 15:00:47 +0100

    Hacky finishing stuff!

---
+++ TDD4/PlayerSelectionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TDD4
+{
+    [TestFixture]
+    public class PlayerSelectionTests
+    {
+        [TestCase("R", Selection.Rock)]
+        [TestCase("S", Selection.Scissors)]
+        [TestCase("P", Selection.Paper)]
+        public void PlayerOneCanInputSelection(string input, Selection expectedSelection)
+        {
+            var round = new Round();
+            round.InputPlayerOneSelection(input);
+            round.PlayerOneSelection.Should().Be(expectedSelection);
+        }
+
+        [TestCase("gibberish")]
+        public void PlayerOneCanInputSelection(string input)
+        {
+            var round = new Round();
+            Assert.Throws<Exception>(() => round.InputPlayerOneSelection(input));           
+        }
+    }
+}
--- TDD4/Round.cs
+++ TDD4/Round.cs
@@ -8,6 +8,44 @@ namespace TDD4
 {
     public class Round
     {
+        public Selection PlayerOneSelection;
+
+        public void InputPlayerOneSelection(string userInput)
+        {
+            switch (userInput)
+            {
+                case "R":
+                    PlayerOneSelection = Selection.Rock;
+                    break;
+                case "S":
+                    PlayerOneSelection = Selection.Scissors;
+                    break;
+                case "P":
+                    PlayerOneSelection = Selection.Paper;
+                    break;
+            }
+            throw new Exception("I ran out of time to do input errors politely");
+        }
+
+        public Selection PlayerTwoSelection;
+
+        public void InputPlayerTwoSelection(string userInput)
+        {
+            switch (userInput)
+            {
+                case "R":
+                    PlayerTwoSelection = Selection.Rock;
+                    break;
+                case "S":
+                    PlayerTwoSelection = Selection.Scissors;
+                    break;
+                case "P":
+                    PlayerTwoSelection = Selection.Paper;
+                    break;
+            }
+            throw new Exception("I ran out of time to do input errors politely");
+        }
+
         public static RoundResult Wins(Selection player1Selection, Selection player2Selection)
         {
             if (player1Selection == player2Selection) return RoundResult.Draw;
