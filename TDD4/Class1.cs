faedf82d8c88bd3b32afd9f12b5ce912948e1b73
MichelleTaylorRG <michelle.taylor@red-gate.com>
2017-05-26 13:20:50 +0100

    First test - rock beats scissors

---
+++ TDD4/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TDD4
+{
+    [TestFixture]
+    public class RockPaperScissorsTests
+    {
+        [Test]
+        public void RockBeatsScissors()
+        {
+            Round.Wins(Selection.Rock, Selection.Scissors).Should().Be(Selection.Rock);
+        }
+
+    }
+
+
+}
---
+++ TDD4/Round.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD4
+{
+    public class Round
+    {
+        public static Selection Wins(Selection rock, Selection scissors)
+        {
+            return Selection.Rock;
+        }
+    }
+}
